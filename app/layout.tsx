7abc2247bad552dcd2ab729cf02ab1953973a516
ermiastsegu <ermiastsegu@yahoo.com>
2024-06-06 16:01:11 +0300

    width layout added.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
-        {children}
+        <div className="mt-10 w-[94%] mx-auto">{children}</div>
       </body>
     </html>
   );
