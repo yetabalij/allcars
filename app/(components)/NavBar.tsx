ddf43864150a772a458a9ab50c3f0ee6bb4d99e2
ermiastsegu <ermiastsegu@yahoo.com>
2024-06-06 14:31:45 +0300

    logo created.

--- app/(components)/NavBar.tsx
+++ app/(components)/NavBar.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
 import React from "react";
+import Logo from "@/public/logo1.png";
 
 const NavBar = () => {
-  return <div>Nav Bar</div>;
+  return (
+    <div className="">
+      <div className="flex justify-between border-b shadow h-20 items-center px-10">
+        <div className="mt-6">
+          <Image alt="logo" src={Logo} width={120} />
+        </div>
+        <div>
+          <input />
+        </div>
+        <div>right component</div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
