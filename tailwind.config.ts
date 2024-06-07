68a122dbb803af27e986f39dfcc1cdaa90357d24
ermiastsegu <ermiastsegu@yahoo.com>
2024-06-07 11:20:05 +0300

    flowbite configured.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,15 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    flowbite.content(),
+  ],
   prefix: "",
   theme: {
     container: {
@@ -74,7 +76,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require("tailwindcss-animate"), flowbite.content()],
+} satisfies Config;
 
-export default config
+export default config;
