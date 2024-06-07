9876dad3fe59404985a9457b4d5c523f8cc4f86e
ermiastsegu <ermiastsegu@yahoo.com>
2024-06-07 14:37:29 +0300

    flowbite navigation implemented.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,37 +1,36 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import Logo from "@/public/logo1.png";
 import { Input } from "./ui/input";
 import { CiSearch } from "react-icons/ci";
 import Link from "next/link";
-import { Button } from "./ui/button";
+import { Button } from "flowbite-react";
+
+import { Navbar } from "flowbite-react";
 
 const NavBar = () => {
   return (
-    <div className="">
-      <div className="flex border-b shadow h-20 items-center justify-between ">
-        {/* Logo Component */}
-        <div className="mt-6">
-          <Image alt="logo" src={Logo} width={120} />
+    <div className="border-b shadow">
+      <Navbar fluid rounded>
+        <Navbar.Brand href="/">
+          <Image src={Logo} alt="logo" width={100} />
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Button>Post Car</Button>
+          <Navbar.Toggle />
         </div>
-        {/* Search Component */}
-        <div className="rounded-3xl border-gray-300 border-solid border-[1px] w-4/12 flex items-center px-2 py-1 shadow-sm">
-          <Input placeholder="Search" />
-          <div className="p-2 bg-blue-800 text-white rounded-full">
-            <CiSearch className="text-xl" />
-          </div>
-        </div>
-        {/* Navigation Component */}
-        <div className="mr-10">
-          <div className="flex gap-4 justify-center items-center text-sm">
-            <Link href={"/about"}>About Us</Link>
-            <Link href={"/contact"}>Contact Us</Link>
-            <Link href={"/signup"}>
-              <Button className="bg-blue-800">Sell A Car</Button>
-            </Link>
-          </div>
-        </div>
-      </div>
+        <Navbar.Collapse>
+          <Navbar.Link href="#" active>
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="#">About</Navbar.Link>
+          <Navbar.Link href="#">Services</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Contact</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
