f9e097d2419bd2c3b4e02b2034608deb3fa8722b
ermiastsegu <ermiastsegu@yahoo.com>
2024-06-07 15:06:16 +0300

    electric car schema added.

---
+++ app/models/car/electric_car.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const { commonAttributes } = require("@/app/models/car/commonattributes");
+
+//Define the EngineCar schema
+
+const electricCareScheam = new mongoose.Schema({
+  ...commonAttributes,
+  battry_capacity: {
+    type: String,
+    required: true,
+  },
+  range: {
+    type: Number,
+    required: true,
+  },
+});
+
+const ElectricCar = mongoose.model("ElectricCar", electricCareScheam);
+module.exports = ElectricCar;
