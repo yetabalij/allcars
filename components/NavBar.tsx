1b7059ddf57f225a8f9e337555d83dee71f1e8b9
ermiastsegu <ermiastsegu@yahoo.com>
2024-08-09 09:41:35 +0300

    button changed.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,6 +5,7 @@ import Logo from "@/public/logo.png";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
 
 const NavBar = () => {
   return (
@@ -16,7 +17,12 @@ const NavBar = () => {
         <div className="hidden lg:flex">search</div>
         <div className="hidden md:flex gap-5 items-center">
           <Link href="/partners/home">
-            <Button variant="ghost">Post a Car</Button>
+            <Button variant="default" size="sm">
+              <div className="mr-1">
+                <Plus size={16} />
+              </div>
+              Post a Car
+            </Button>
           </Link>
           <div>localization</div>
           <div>menu</div>
